12580df69e2342f4586996caa1fd199674419a89
Luis J. Centeno <luisjcenteno@live.com>
2024-09-27 16:38:42 -0600

    Basic production docker setup

--- photos/settings.py
+++ photos/settings.py
@@ -99,7 +99,7 @@ DATABASES = {
         'NAME': os.environ.get('POSTGRES_NAME'),
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': 'database',
+        'HOST': os.environ.get('POSTGRES_HOST'),
         'PORT': 5432,
     }
 }
@@ -170,6 +170,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
+
+MEDIA_URL = '/uploads/'
 
 APPEND_SLASH = True
 
