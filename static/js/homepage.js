c286b3266c9be9e3b448a219a020dcbeef688827
Luis J. Centeno <luisjcenteno@live.com>
2024-10-02 13:52:41 -0600

    Update homepage and readme

--- static/js/homepage.js
+++ static/js/homepage.js
@@ -5,18 +5,29 @@ function scrollToSecion(id) {
 
 document.addEventListener('DOMContentLoaded', () => {
     const backToTopButton = document.getElementById('back-to-top')
+    const $heroSection = document.getElementById('main-hero-section')
+    const $heroVideo = document.getElementById('hero-video-player')
+
     backToTopButton.addEventListener('click', () => {
         window.scrollTo({
             top: 0,
             behavior: 'smooth'
         })
     })
-})
 
-window.addEventListener('scroll', () => {
-    if (window.scrollY > 100) {
-        document.querySelector('.back-to-top').style.display = 'flex'
-    } else {
-        document.querySelector('.back-to-top').style.display = 'none'
-    }
+    window.addEventListener('scroll', () => {
+        // Back to top button
+        if (window.scrollY > 100) {
+            document.querySelector('.back-to-top').style.display = 'flex'
+        } else {
+            document.querySelector('.back-to-top').style.display = 'none'
+        }
+
+        // Stop video when scrolling
+        if (window.scrollY > $heroSection.offsetHeight - 100) {
+            $heroVideo.pause()
+        } else {
+            $heroVideo.play()
+        }
+    })
 })
