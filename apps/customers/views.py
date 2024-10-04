6de24213cfc8e65274c7da0b12892e2d0a24dab6
Luis J. Centeno <luisjcenteno@live.com>
2024-10-04 09:42:46 -0600

    Require login for customers admin

--- apps/customers/views.py
+++ apps/customers/views.py
@@ -2,22 +2,23 @@ from .models import Customer
 from .forms import CustomerForm
 from django.views.generic import ListView, FormView, UpdateView
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class CustomerListView(ListView):
+class CustomerListView(LoginRequiredMixin, ListView):
     model = Customer
     template_name = 'customer_list.html'
     context_object_name = 'customers'
 
 
-class CustomerCreateView(FormView):
+class CustomerCreateView(LoginRequiredMixin, FormView):
     model = Customer
     form_class = CustomerForm
     template_name = 'customer_create.html'
     success_url = reverse_lazy('customers:list_customers')
 
 
-class CustomerEditView(UpdateView):
+class CustomerEditView(LoginRequiredMixin, UpdateView):
     model = Customer
     form_class = CustomerForm
     template_name = 'customer_create.html'
